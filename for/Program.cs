2349a9cd08bc165f8cadfcb7013ab04c8285afd3
Mauricio Mejia Estevez <mme0504@gmail.com>
2018-08-07 14:01:58 -0500

    Add files via upload

---
+++ for/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace @for
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            for (byte i = 1; i <= 10; i++)
+            {
+                Console.WriteLine(+i);
+
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
